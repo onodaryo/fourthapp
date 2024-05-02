c8ebc7eef86e09bb34c1be454fd32dbd7f0b07ca
ryoonoda <onoda.ryo@genetec.co.jp>
2024-05-02 14:48:31 +0900

    食事ジャンルを押すとそのジャンル一覧を表示する機能の追加

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -3,10 +3,16 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:meal/data/dummyData.dart';
 import 'package:meal/models/category.dart';
+import 'package:meal/screens/meals.dart';
 import 'package:meal/widgets/categoryGridItem.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category,){
+    final filteredMeals = dummyMeals.where((meal) => meal.categories.contains(category.id)).toList();
+    Navigator.push(context, MaterialPageRoute(builder: (ctx) => MealScreen(title: category.title, meals: filteredMeals)));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +30,12 @@ class CategoriesScreen extends StatelessWidget {
         ),
         children: [
           for(final category in availableCategories )
-            CategoryGridItem(category: category)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: (){
+                _selectCategory(context, category);
+              },
+            ),
         ],
       ),
     );
