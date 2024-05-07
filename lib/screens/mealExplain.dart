8dcf396599273b827bc756de07a9a42643085cb6
ryoonoda <onoda.ryo@genetec.co.jp>
2024-05-07 10:12:22 +0900

    料理個別ページに材料と調理手順の追加

--- lib/screens/mealExplain.dart
+++ lib/screens/mealExplain.dart
@@ -10,40 +10,53 @@ class MealExplainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Widget content = Image.network(
-      meal.imageUrl,
-      height: 300,
-      width: double.infinity,
-      fit: BoxFit.cover,
-    );
-
-    if (meal == null) {
-      content = Center(
-        child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                'Uh oh ... nothing here!?!?!?!?!?',
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineLarge!
-                    .copyWith(color: Theme.of(context).colorScheme.onBackground),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              Text('Try selecting a different meal...',
-                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                  color: Theme.of(context).colorScheme.onBackground,
-                ),)
-            ]),
-      );
-    }
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: content,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.network(
+              meal.imageUrl,
+              height: 300,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 14,),
+            Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(color: Theme.of(context).colorScheme.primary,fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 14,),
+            for(final ingredient in meal.ingredients)
+              Text(
+                ingredient,
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(color: Theme.of(context).colorScheme.onBackground),
+              ),
+            const SizedBox(height: 24,),
+            Text(
+              'Steps',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(color: Theme.of(context).colorScheme.primary,fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 14,),
+            for(final step in meal.steps)
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 8,
+                ),
+                child: Text(
+                  step,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.titleMedium!.copyWith(color: Theme.of(context).colorScheme.onBackground),
+                ),
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
